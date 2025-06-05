defb9aa5cb2f6d25989aa3395b5a284decf04f5d
Makena Kong <kena.kong@gmail.com>
2025-06-05 02:25:52 -0700

    added new office photos and updated gallery

--- pages/About-Us/Gallery.vue
+++ pages/About-Us/Gallery.vue
@@ -7,10 +7,18 @@
       During your visit, enjoy our massage chairs and unwind with your choice of a movie or music as we take care of
       your smile.
     </p>
-    <div class="grid md:grid-cols-3 gap-8 py-4">
-      <img src="/images/office/old-waiting-room.webp" class="w-64 h-64 mx-auto object-cover rounded" />
-      <img src="/images/office/old-computers.webp" class="w-64 h-64 mx-auto object-cover rounded" />
-      <img src="/images/office/old-hallway.webp" class="w-64 h-64 mx-auto object-cover rounded" />
+    <div class="grid grid-cols-1 sm:grid-cols-3 grid-rows-masonry gap-4">
+      <div class="sm:col-span-2">
+        <img src="/images/office/waiting-room.webp" class="w-full h-auto object-cover rounded" loading="lazy" />
+      </div>
+      <div class="sm:col-span-1">
+        <img src="/images/office/front-door.webp" class="w-full h-full object-cover rounded" loading="lazy" />
+      </div>
+    </div>
+    <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
+      <img src="/images/office/patient-room.webp" class="h-full w-full mx-auto object-cover rounded" loading="lazy" />
+      <img src="/images/office/patient-chair.webp" class="h-full w-full mx-auto object-cover rounded" loading="lazy" />
+      <img src="/images/office/hallway.webp" class="h-full w-full mx-auto object-cover rounded" loading="lazy" />
     </div>
   </UiSection>
 </template>
