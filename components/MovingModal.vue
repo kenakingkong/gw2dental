c70e724ecbbae75103f9d797d9104ec0efc15d98
Makena Kong <kena.kong@gmail.com>
2025-06-05 02:26:08 -0700

    updated moving modal

--- components/MovingModal.vue
+++ components/MovingModal.vue
@@ -1,13 +1,9 @@
 <template>
   <UiModal>
     <div class="space-y-4 text-lg text-black">
-      <h2 class="text-2xl md:text-4xl font-medium">We are moving offices!</h2>
-      <p class="font-medium">Our office will be closed from Tuesday, April 29 to Sunday May 11.</p>
-      <p>Our new office, located at <span class="font-medium">12677 Alcosta Blvd Suite 415</span> will be open to new
-        patients starting Monday May 12.
-      </p>
-      <p>We will continue to see patients at our current office at 2301 Camino Ramon, Suite 100 until April 29.</p>
-      <p>Thanks for your cooperation and patience!</p>
+      <h2 class="text-2xl md:text-4xl font-medium">We moved offices!</h2>
+      <p>Our new office is located at <span class="font-medium">12677 Alcosta Blvd Suite 415</span>.</p>
+      <p>See you at your next appointment!</p>
       <p>Dr. Wong & Dr. Wu</p>
     </div>
   </UiModal>
